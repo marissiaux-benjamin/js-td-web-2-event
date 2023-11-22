0534945cd92df0379c999e6b8cc34dfc368f7cc7
benjamin <benjamin.marissiaux@student.hepl.be>
2023-11-22 12:46:29 +0100

    script.js

--- starter/07-change-couleur-4/script.js
+++ starter/07-change-couleur-4/script.js
@@ -6,3 +6,30 @@ Version 1  : une fonction par bouton radio
 		(1) un alert dans une fonction pour voir à quoi correspond e.target
 		(2) un alert dans chaque fonction pour voir à quoi correspond e.target.value
 */
+
+const couleur = {
+    colorchange() {
+        const inputSelect = document.querySelectorAll('input[type = radio]');
+        inputSelect.forEach((button) => {
+            button.addEventListener('click', (event) => {
+                document.body.style.backgroundColor = event.currentTarget.value;
+            })
+        });
+    },
+    randomColor() {
+        const buttonSelect = document.getElementById('boutonCouleur');
+        buttonSelect.addEventListener('click', (l) => {
+            document.body.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255 + 1)}, ${Math.floor(Math.random() * 255 + 1)}, ${Math.floor(Math.random() * 255 + 1)})`;
+        })
+    }
+}
+couleur.colorchange();
+couleur.randomColor();
+
+
+
+
+
+
+
+
